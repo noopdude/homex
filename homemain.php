64f66717e49c902bdb0176c0ad7ee65232f55837
noopdude <anoop.k.phil@gmail.com>
2021-03-25 18:48:05 +0000

    home management changes

--- homemain.php
+++ homemain.php
@@ -97,12 +97,12 @@
 
                               echo    "
                                            <tr>
-                                               <td>".$row['home_name']."</td>
+                                               <td> <input type=\"text\" name=\"home_name[]\" value=\"" .$row['home_name']. "\"></td>
                                                <td>".$row['home_type']."</td>
-                                               <td>".$row['status']."</td>
-                                               <td>".$row['home_owner_username']."</td>
-                                               <td>".$row['maintenance_dues']."</td>
-                                               <td>".$row['other_dues']."</td>
+                                               <td> <input type=\"text\" name=\"home_status[]\" value=\"" .$row['status']. "\"></td>
+                                               <td> <input type=\"text\" name=\"home_owner_username[]\" value=\"" .$row['home_owner_username']. "\"></td>
+                                               <td> <input type=\"number\" name=\"maintenance_dues[]\" value=\"" .$row['maintenance_dues']. "\"></td>
+                                               <td> <input type=\"number\" name=\"other_dues[]\" value=\"" .$row['other_dues']. "\"></td>
                                            </tr>
                                        ";
 
@@ -111,6 +111,7 @@
                           mysqli_close($db);
 
                     ?>
+
                     <ul class="pagination">
 
                     <li><a href="?pageno=1">First</a></li>
@@ -123,12 +124,59 @@
                     <li><a href="?pageno=<?php echo $total_pages; ?>">Last</a></li>
                     </ul>
 
-
+                    <input type="submit" class="button" style="float: right; margin-top: 10px" name="update_home" value="Update" >
               </form>
           </div>
 
           <div class="footer">
           <p>Home-X Beta Version | Engineered by LIAN | Powered by AWS</p>
           </div>
+          <?php
+                if(isset($_POST['update_home'])){
+                      $home_name = $_POST['home_name'];
+                      $home_status = $_POST['home_status'];
+                      $home_owner_username = $_POST['home_owner_username'];
+                      $maintenance_dues = $_POST['maintenance_dues'];
+                      $other_dues = $_POST['other_dues'];
+                      $username = $_SESSION['username'];
+                      $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
+
+                      $arrayLength =  count($home_owner_username);
+
+                      if ( $arrayLength > 0 ){
+                            UpdateHome($db,$home_name, $home_status, $home_owner_username, $maintenance_dues, $other_dues, $username );
+                      }
+                }
+
+          ?>
       </body>
 </html>
+<?php
+function UpdateHome($db, $home_name, $home_status,  $home_owner_username,$maintenance_dues,$other_dues,$username ) {
+
+        $errors = array();
+        $i = 0;
+        $arrayLength =  count($home_status);
+        $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
+        while ($i < $arrayLength)
+        {
+              echo $home_name[$i];
+              $query = "UPDATE homex.homes SET status = '$home_status[$i]', home_owner_username = '$home_owner_username[$i]' , maintenance_dues = $maintenance_dues[$i] , other_dues = $other_dues[$i] WHERE home_name = '$home_name[$i]' ";
+              mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+              if(!mysqli_query($db, $query)) {
+                  array_push($errors, "Error updating Home: $home_name[$i] ");
+              }
+              else{
+                  //echo "<h3>Thank you! Selected Payment Requests have been closed</h3>";
+              }
+              $i++;
+        }
+        if (count($errors) == 0){
+              echo "<h3>Thank you! Changes have been applied</h3>";
+        }
+        else{
+              echo "Some records failed to update";
+        }
+        mysqli_close($db);
+}
+ ?>
